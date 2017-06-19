e61cae7d167239f9f6451ff8e74ee38b22e08de5
farnaz <farnaz.rashidi@gmail.com>
2017-06-18 17:43:47 -0700

    Added specs for building a form, creating a new book and saving it to db

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,7 @@
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
+post '/books', to: 'books#create'
+get '/books/new', to: 'books#new'
 get '/books', to: 'books#index'
 
 root to: 'home#index'
