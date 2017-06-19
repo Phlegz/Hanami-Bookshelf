b5ebb7b36b19de36fe0f26ffaa1d63bfd7693198
farnaz <farnaz.rashidi@gmail.com>
2017-06-18 19:03:16 -0700

    Applied the resource helper method in routes.rb and modified the path in the code using routes helper method

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,5 @@
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
-post '/books', to: 'books#create'
-get '/books/new', to: 'books#new'
-get '/books', to: 'books#index'
 
 root to: 'home#index'
+resources :books, only: [:index, :new, :create]
