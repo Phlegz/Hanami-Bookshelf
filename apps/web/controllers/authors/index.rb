7d1d5d8cd7861f2921f57acadbc253d280deecb9
farnaz <farnaz.rashidi@gmail.com>
2017-06-21 14:50:28 -0700

    Order the authors based on the number of books they have in db

--- apps/web/controllers/authors/index.rb
+++ apps/web/controllers/authors/index.rb
@@ -2,7 +2,11 @@ module Web::Controllers::Authors
   class Index
     include Web::Action
 
+    expose :authors
+
     def call(params)
+      @authors = AuthorRepository.new.order_by_book_number
     end
+
   end
 end
