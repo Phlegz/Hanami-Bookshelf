0308e81b8794180eeda00d9219e2b37dbe2673fc
farnaz <farnaz.rashidi@gmail.com>
2017-06-21 14:43:39 -0700

    Added a method to the Author repository

--- lib/bookshelf/repositories/author_repository.rb
+++ lib/bookshelf/repositories/author_repository.rb
@@ -13,4 +13,9 @@ class AuthorRepository < Hanami::Repository
   def find_with_books(id)
     aggregate(:books).where(id: id).as(Author).one
   end
+
+  def order_by_book_number
+    authors.read("select authors.id as id, authors.name as name, count(authors.name) as total from authors join books on authors.id = books.author_id group by authors.id order by count(authors.name) desc").to_a
+  end
+
 end
