df43057e6fe9609db8beee25f0c24651a397b0ab
farnaz <farnaz.rashidi@gmail.com>
2017-06-22 04:12:04 -0700

    Refactored the function that saves the author to db when creating a new book

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -13,14 +13,6 @@ module Web::Controllers::Books
 
     def call(params)
       if params.valid?
-        AuthorRepository.new.all.each do |author|
-          if author.name.upcase == (params[:book])[:author].upcase
-            BookRepository.new.create(author_id: author.id,title: (params[:book])[:title] )
-            redirect_to routes.books_path
-            break
-          end
-        end
-
         AuthorRepository.new.create_with_books(name: (params[:book])[:author], books: [{title: (params[:book])[:title]}])
         redirect_to routes.books_path
       else
--- lib/bookshelf/repositories/author_repository.rb
+++ lib/bookshelf/repositories/author_repository.rb
@@ -5,8 +5,15 @@ class AuthorRepository < Hanami::Repository
 
 # create an author with a collection of books:
 # invoke the function in this way: create_with_books(name: "AuthorName", books: [{title: "Booktitle"}])
+#This function is invoked when creating a new book and inserts the associated author to the db/ It also checks if the author already exists in the db and if it does, it just saves the book with the corresponding author_id
   def create_with_books(data)
+    result = authors.read("select name from authors where name='#{data[:name]}'").to_a
+    if ((result[0]) != nil)
+      result = authors.read("select id from authors where name='#{data[:name]}'").to_a
+      BookRepository.new.create(author_id: result[0].id, title: (data[:books][0])[:title])
+    else
     assoc(:books).create(data)
+    end
   end
 
 #find the author whose id matches the author_id from the books table
