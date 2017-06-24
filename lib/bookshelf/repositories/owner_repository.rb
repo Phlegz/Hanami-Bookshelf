e71ef156ccc3bb7e0ad5886c8250a68cb22d06c3
farnaz <farnaz.rashidi@gmail.com>
2017-06-24 00:16:09 -0700

    Added some methods to the ownerRepository to help find and create a book

--- lib/bookshelf/repositories/owner_repository.rb
+++ lib/bookshelf/repositories/owner_repository.rb
@@ -2,4 +2,13 @@ class OwnerRepository < Hanami::Repository
   associations do
     has_many :books
   end
+
+  def create_with_books(data)
+    assoc(:books).create(data)
+  end
+
+  def find_with_books(id)
+    aggregate(:books).where(id: id).as(Owner).one
+  end
+
 end
