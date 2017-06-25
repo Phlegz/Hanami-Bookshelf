62e3fb5c2156d315bab1770370ab1f3f0b63b6fd
farnaz <farnaz.rashidi@gmail.com>
2017-06-25 03:50:33 -0700

    Added a method to the owner repository to check whether owner exists or not

--- lib/bookshelf/repositories/owner_repository.rb
+++ lib/bookshelf/repositories/owner_repository.rb
@@ -3,6 +3,11 @@ class OwnerRepository < Hanami::Repository
     has_many :books
   end
 
+# invoke the function in this way: create_with_books(owner: "OwnerName", books: [{title: "Booktitle"}])
+  def owner_exists(data)
+    owners.read("select id from owners where name='#{data}'").to_a
+  end
+
   def create_with_books(data)
     assoc(:books).create(data)
   end
